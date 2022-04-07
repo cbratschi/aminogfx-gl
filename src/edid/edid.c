e3adeaa0d4dd47fa1d5a69068f759924b450e81c
Christoph Bratschi <bratschi@appamics.com>
2022-04-07 18:18:51 +0200

    more

--- src/edid/edid.c
+++ src/edid/edid.c
@@ -174,6 +174,9 @@ struct edid_info *edid_parse(const uint8_t *data, size_t length) {
 
     if (serial_number > 0) {
         sprintf(edid->serial_number, "%lu", (unsigned long) serial_number);
+
+        //debug cbxx
+        printf("-> serial number: %s\n", edid->serial_number);
     }
 
     //week and year
